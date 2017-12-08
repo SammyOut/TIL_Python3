a902f1d2a8bde176c9aa2be3ea2ee18a1ae895cf
Sangmin <sm372181@gmail.com>
2017-12-08 20:03:13 +0900

    [ADD] 001_HelloWorld!

---
+++ 001_HelloWorld!/HelloWorld!.py
@@ -0,0 +1,9 @@
+# 파이썬에서 출력을 할 때는 print 함수를 사용한다.
+print("Hello World!")
+# 파이썬에서 문자열을 사용할 때 '', "", ''' ''', """ """을 사용할 수 있다.
+print('Hello World!')
+
+# 파이썬에서 print 함수를 사용하면 출력 후 자동으로 개행이 된다.
+# print 함수 사용할 때 자동 개행을 없애고 싶다면 print 함수의 parameter 중 end를 사용하면 된다.
+print('Hello ', end='')
+print('World')
