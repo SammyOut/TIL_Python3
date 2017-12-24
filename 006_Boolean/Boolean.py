7dd485e091eabcdc291c73815fc136597ff60049
Sangmin <sm372181@gmail.com>
2017-12-24 16:08:01 +0900

    [ADD] 006_Boolean

---
+++ 006_Boolean/Boolean.py
@@ -0,0 +1,14 @@
+# Boolan은 참인지 거짓인지를 나타내는 식이다.
+# 참이면 True 거짓이면 False를 나타낸다.
+booleanA = True
+booleanB = False
+print('booleanA type: {0}, value: {1}'.format(type(booleanA), booleanA))
+print('booleanB type: {0}, value: {1}'.format(type(booleanB), booleanB))
+
+# 보통 관계연산자로 boolean을 많이 사용한다.
+print('10 < 20: {0}', 10 < 20)
+print('10 <= 20: {0}', 10 <= 20)
+print('10 > 20: {0}', 10 > 20)
+print('10 >= 20: {0}', 10 >= 20)
+print('10 == 20: {0}', 10 < 20)
+print('10 < 20: {0}', 10 < 20)
